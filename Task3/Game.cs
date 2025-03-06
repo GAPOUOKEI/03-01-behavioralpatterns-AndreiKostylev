bce497d4a8c9be33a35f3b601fe65c751b1ba798
Андреев Андрей <kostandr06@icloud.com>
2025-03-06 13:22:48 +0500

    практика выполнена

---
+++ Task3/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Game
+    {
+        private List<string> moves = new List<string>();
+        public int Turn { get; private set; } = 1;
+
+        public void MakeMove(string move)
+        {
+            moves.Add($"Ход {Turn}: {move}");
+            Console.WriteLine($"Выполнен ход {Turn}: {move}");
+            Turn++;
+        }
+
+        public void UndoMove()
+        {
+            if (moves.Count > 0)
+            {
+                string lastMove = moves[^1];
+                moves.RemoveAt(moves.Count - 1);
+                Turn--;
+                Console.WriteLine($"Отменён {lastMove}");
+            }
+            else
+            {
+                Console.WriteLine("Нет ходов для отмены.");
+            }
+        }
+
+        public void ShowMoves()
+        {
+            Console.WriteLine("\nИстория ходов:");
+            if (moves.Count == 0)
+                Console.WriteLine("Пока нет ходов.");
+            else
+                moves.ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ Task3/GameInvoker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class GameInvoker
+    {
+        private Stack<ICommand> commandHistory = new Stack<ICommand>();
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            commandHistory.Push(command);
+        }
+
+        public void UndoLastCommand()
+        {
+            if (commandHistory.Count > 0)
+            {
+                ICommand lastCommand = commandHistory.Pop();
+                lastCommand.Undo();
+            }
+            else
+            {
+                Console.WriteLine("Нет команд для отмены.");
+            }
+        }
+    }
+}
